f3cc1debb3cb4a323a675a8fd5f691cc30b16173
Sin <sinappeal@gmail.com>
2024-10-25 09:04:23 +0530

    Some fixes to access the admin side

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/treatment-stages': {
-        target: 'https://rilcet.onrender.com', // Backend server URL
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // If using HTTPS, set this to true
+      "/treatment-stages": {
+        target: "https://rilcet.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
-      '/evaluation': {
-        target: 'https://rilcet.onrender.com', // Backend server URL
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // If using HTTPS, set this to true
+      "/evaluation": {
+        target: "https://rilcet.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optional, to control chunking behavior
+      },
+    },
+  },
+});
