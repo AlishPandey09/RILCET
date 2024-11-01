f95fa0346b0ad9ea1d9b549f62839b2907a7492b
Sin <sinappeal@gmail.com>
2024-11-01 23:05:05 +0530

    Applying the socket.io function to enable the realtime fetching

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const treatmentStageRoutes = require('./routes/treatmentStageRoutes');
 const evaluationResultRoutes = require("./routes/evaluationResultRoutes");
+const http = require("http");
+const { Server } = require('socket.io');
 require('dotenv').config();
 
 const app = express();
@@ -16,9 +18,25 @@ connectDB();
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 
+// Create HTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.io
+const io = new Server(server);
+
 // Use the treatment stages routes
 app.use('/treatment-stages', treatmentStageRoutes);
 app.use('/evaluation', evaluationResultRoutes);
+
+// Socket.IO event handling
+io.on('connection', (socket) => {
+  console.log("New client connected");
+
+  // handle disconnection
+  socket.on('disconnect', () => {
+    console.log("Client disconnected");
+  });
+});
 
 // Start the server
 app.listen(PORT, () => {
